dfd244e893d33fcfdace8bfee642e3af5f824cd6
txavier <txavier@gmail.com>
2022-11-01 20:30:19 -0400

    Kill any previous running version of this program in case a previous instance has become stuck.

--- robinhoodbot/run.sh
+++ robinhoodbot/run.sh
@@ -4,6 +4,9 @@
 while true; do
    # do stuff
    echo $(date)
+   # https://stackoverflow.com/questions/40652793/how-to-kill-python-script-with-bash-script
+   pkill -f main.py
    python3 main.py
+   echo $(date)
    sleep $[60 * 30]
 done
